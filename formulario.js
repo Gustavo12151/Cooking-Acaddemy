31677a4d587d2ea9608405fde48b576cb48a7282
Gustavo12151 <gustavorosales2199@gmail.com>
2023-10-30 15:59:24 -0300

    .

--- formulario.js
+++ formulario.js
@@ -1,42 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
     let form = document.getElementById("registro-form");
     let resultado = document.getElementById("resultado");
+    let nombreInput = document.getElementById("nombre");
+    let apellidoInput = document.getElementById("apellido");
+    let correoInput = document.getElementById("correo");
+    let cursoInput = document.getElementById("curso");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        let nombre = document.getElementById("nombre").value;
-        let apellido = document.getElementById("apellido").value;
-        let correo = document.getElementById("correo").value;
-        let curso = document.getElementById("curso").value;
+        let nombre = nombreInput.value;
+        let apellido = apellidoInput.value;
+        let correo = correoInput.value;
+        let curso = cursoInput.value;
 
         let emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-        if (nombre === "" || apellido === "" || correo === "" || curso === "") {
-            mostrarMensajeError("Por favor, complete todos los campos.");
+        // Limpiar errores previos
+        let errorElements = document.getElementsByClassName("error");
+        for (let i = 0; i < errorElements.length; i++) {
+            errorElements[i].textContent = "";
+        }
+
+        if (nombre === "") {
+            document.getElementById("nombre-error").textContent = "Campo requerido";
+        }
+        if (apellido === "") {
+            document.getElementById("apellido-error").textContent = "Campo requerido";
+        }
+        if (correo === "") {
+            document.getElementById("correo-error").textContent = "Campo requerido";
         } else if (!emailRegex.test(correo)) {
-            mostrarMensajeError("El correo electrónico no es válido.");
-        } else {
+            document.getElementById("correo-error").textContent = "El correo electrónico no es válido";
+        }
+        if (curso === "") {
+            document.getElementById("curso-error").textContent = "Seleccione un curso";
+        }
+
+        if (nombre !== "" && apellido !== "" && correo !== "" && curso !== "" && emailRegex.test(correo)) {
             mostrarMensajeExito(nombre, apellido, correo, curso);
         }
     });
 
-    function mostrarMensajeError(mensaje) {
-        resultado.innerHTML = ""; // Limpiar cualquier resultado anterior
-        let mensajeError = document.createElement("div");
-        mensajeError.className = "error";
-        mensajeError.textContent = mensaje;
-        resultado.appendChild(mensajeError);
-        resultado.style.display = "block";
-    }
-
     function mostrarMensajeExito(nombre, apellido, correo, curso) {
-        resultado.innerHTML = ""; // Limpiar cualquier resultado anterior
-        let mensajeExito = document.createElement("div");
-        mensajeExito.className = "exito";
-        mensajeExito.innerHTML = `Registro exitoso:<br>Nombre: ${nombre}<br>Apellido: ${apellido}<br>Correo: ${correo}<br>Curso: ${curso}`;
-        resultado.appendChild(mensajeExito);
-        resultado.style.display = "block";
+        resultado.innerHTML = `Registro exitoso:<br>Nombre: ${nombre}<br>Apellido: ${apellido}<br>Correo: ${correo}<br>Curso: ${curso}`;
+        // Limpiar campos y mensajes de error
+        nombreInput.value = "";
+        apellidoInput.value = "";
+        correoInput.value = "";
+        cursoInput.value = "";
+        let errorElements = document.getElementsByClassName("error");
+        for (let i = 0; i < errorElements.length; i++) {
+            errorElements[i].textContent = "";
+        }
     }
 });
+
+
 
