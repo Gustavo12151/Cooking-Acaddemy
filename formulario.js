fb34cebb79a710c80cdae5aad777295aae23055d
Gustavo12151 <gustavorosales2199@gmail.com>
2023-10-30 13:59:41 -0300

    .

--- formulario.js
+++ formulario.js
@@ -1,18 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("registro-form");
-    const resultado = document.getEle
-    
-    mentById("resultado");
+    let form = document.getElementById("registro-form");
+    let resultado = document.getElementById("resultado");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
 
-        const nombre = document.getElementById("nombre").value;
-        const apellido = document.getElementById("apellido").value;
-        const correo = document.getElementById("correo").value;
-        const curso = document.getElementById("curso").value;
+        let nombre = document.getElementById("nombre").value;
+        let apellido = document.getElementById("apellido").value;
+        let correo = document.getElementById("correo").value;
+        let curso = document.getElementById("curso").value;
 
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        let emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
         if (nombre === "" || apellido === "" || correo === "" || curso === "") {
             mostrarMensajeError("Por favor, complete todos los campos.");
@@ -25,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function mostrarMensajeError(mensaje) {
         resultado.innerHTML = ""; // Limpiar cualquier resultado anterior
-        const mensajeError = document.createElement("div");
+        let mensajeError = document.createElement("div");
         mensajeError.className = "error";
         mensajeError.textContent = mensaje;
         resultado.appendChild(mensajeError);
@@ -34,10 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function mostrarMensajeExito(nombre, apellido, correo, curso) {
         resultado.innerHTML = ""; // Limpiar cualquier resultado anterior
-        const mensajeExito = document.createElement("div");
+        let mensajeExito = document.createElement("div");
         mensajeExito.className = "exito";
         mensajeExito.innerHTML = `Registro exitoso:<br>Nombre: ${nombre}<br>Apellido: ${apellido}<br>Correo: ${correo}<br>Curso: ${curso}`;
         resultado.appendChild(mensajeExito);
         resultado.style.display = "block";
     }
 });
+
